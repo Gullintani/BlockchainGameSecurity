96681159cbaf3988b04d1b62f6db246c9baad4e7
Gullintani <116010168@link.cuhk.edu.cn>
2019-03-23 01:57:19 +0800

    Chage: reconstruct input and output directory, delete useless files, update scanning.py

--- scanning.py
+++ scanning.py
@@ -1,21 +1,33 @@
-import csv, os, json
-
-inputCSV = open("./game_contract_address/name&address.csv", "r")
+import csv, os, json, gc
+gc.disable()
+inputCSV = open("./input/name&address2.csv", "r")
 reader = csv.reader(inputCSV)
 
-outputCSV = open("./game_result.csv", "w")
+outputCSV = open("./output/game_result2.csv", "w")
 writer = csv.writer(outputCSV)
 # 
-count = 0
+
 for item in reader:
-    output = os.popen("sudo docker run mythril/myth -xo json -a " + item[1], "r")
+    output = ""
+    output = os.popen("docker run mythril/myth -xo json -a " + item[1], "r")
+    if output == None:
+        del output
+        gc.collect()
+        continue
     result_dic = json.loads(output.read())
     if result_dic["issues"] == []:
         writer.writerow([item[0],item[1]])
         print(reader.line_num)
+        del output
+        del result_dic
+        gc.collect()
         continue
     else:
         for issue in result_dic["issues"]:
-            row = [item[0], item[1], issue["address"], issue["title"], issue["severity"], issue["function"], issue["sourceMap"], issue["swc-id"], issue["max_gas_used"], issue["min_gas_used"], issue["debug"], issue["description"]]
+            row = [item[0], item[1], issue["address"], issue["title"], issue["severity"], issue["function"], issue["sourceMap"], issue["swc-id"], issue["max_gas_used"], issue["min_gas_used"]]
             writer.writerow(row)
-    print(reader.line_num)
+        del output
+        del result_dic
+        del row
+        gc.collect()
+        print(reader.line_num)
