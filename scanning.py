1632e763d23c40776520d41d9ce10cc293e00bb6
Gullintani <116010168@link.cuhk.edu.cn>
2019-03-23 21:44:39 +0800

    Update: scanning.py

--- scanning.py
+++ scanning.py
@@ -6,28 +6,30 @@ reader = csv.reader(inputCSV)
 outputCSV = open("./output/game_result2.csv", "w")
 writer = csv.writer(outputCSV)
 # 
+def get_result():
+    for item in reader:
+        try:
+            output = ""
+            output = os.popen("docker run mythril/myth -xo json -a " + item[1] + " --execution-timeout 60", "r")
+            if output == None:
+                del output
+                continue
+            result_dic = json.loads(output.read())
+            if result_dic["issues"] == []:
+                writer.writerow([item[0],item[1]])
+                print(reader.line_num)
+                del output
+                del result_dic
+                continue
+            else:
+                for issue in result_dic["issues"]:
+                    row = [item[0], item[1], issue["address"], issue["title"], issue["severity"], issue["function"], issue["sourceMap"], issue["swc-id"], issue["max_gas_used"], issue["min_gas_used"]]
+                    writer.writerow(row)
+                del output
+                del result_dic
+                del row
+                print(reader.line_num)
+        except:
+            continue
 
-for item in reader:
-    output = ""
-    output = os.popen("docker run mythril/myth -xo json -a " + item[1], "r")
-    if output == None:
-        del output
-        gc.collect()
-        continue
-    result_dic = json.loads(output.read())
-    if result_dic["issues"] == []:
-        writer.writerow([item[0],item[1]])
-        print(reader.line_num)
-        del output
-        del result_dic
-        gc.collect()
-        continue
-    else:
-        for issue in result_dic["issues"]:
-            row = [item[0], item[1], issue["address"], issue["title"], issue["severity"], issue["function"], issue["sourceMap"], issue["swc-id"], issue["max_gas_used"], issue["min_gas_used"]]
-            writer.writerow(row)
-        del output
-        del result_dic
-        del row
-        gc.collect()
-        print(reader.line_num)
+get_result()
